9c4227b774522015c97c08bf54e294568c5d7c94
StellarWolfEntertainment <stellarwolfentertainment@gmail.com>
2024-08-27 13:48:53 -0600

    Add project files.

---
+++ Aurora/Vector2.cs
@@ -0,0 +1,35 @@
+﻿namespace Aurora;
+
+public partial struct Vector2
+{
+    #region Fields
+
+    public float X;
+    public float Y;
+
+    #endregion
+
+    #region Properties
+
+    public readonly float Length => Math.Sqrt(LengthSquared);
+
+    public readonly float LengthSquared => (X * X) + (Y * Y);
+
+    #endregion
+
+    #region Constructors
+
+    public Vector2(float value)
+    {
+        X = value;
+        Y = value;
+    }
+
+    public Vector2(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    #endregion
+}
